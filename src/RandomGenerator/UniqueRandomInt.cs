cb6677a66efa87fe73ba1f16bdf6ad62a805c10c
Ryddag <27833747+Ryddag@users.noreply.github.com>
2020-02-04 00:08:36 +0100

    Update UniqueRandomGenerator

--- src/RandomGenerator/UniqueRandomInt.cs
+++ src/RandomGenerator/UniqueRandomInt.cs
@@ -7,26 +7,46 @@ using JetBrains.Annotations;
 namespace ProjectCeleste.Misc.Utils.RandomGenerator
 {
     [UsedImplicitly]
+    public class UniqueRandomIntGeneratorAlt
+    {
+        private  IList<int> _excludeNumbers;
+        private readonly UniqueRandomIntGenerator _uniqueRandomIntGenerator;
+
+        public UniqueRandomIntGeneratorAlt([Range(int.MinValue, int.MaxValue - 1)]
+            int min = int.MinValue, [Range(int.MinValue + 1, int.MaxValue)]
+            int max = int.MaxValue,
+            [CanBeNull] IEnumerable<int> excludeNumbers = null)
+        {
+            _uniqueRandomIntGenerator = new UniqueRandomIntGenerator(min, max);
+            _excludeNumbers = excludeNumbers as List<int> ?? excludeNumbers?.ToList() ?? new List<int>();
+        }
+
+        [UsedImplicitly]
+        [Pure]
+        public int Next()
+        {
+           return _uniqueRandomIntGenerator.Next(ref _excludeNumbers);
+        }
+    }
+
+    [UsedImplicitly]
     public class UniqueRandomIntGenerator
     {
         private readonly int _randomMaxReTry = 25000 * Environment.ProcessorCount;
         private readonly object _syncLock = new object();
-        private readonly IList<int> _excludeNumbers;
         private readonly int _minValue;
         private readonly int _maxValue;
         private Random _random;
 
         public UniqueRandomIntGenerator([Range(int.MinValue, int.MaxValue - 1)]
             int min = int.MinValue, [Range(int.MinValue + 1, int.MaxValue)]
-            int max = int.MaxValue,
-            [CanBeNull] IEnumerable<int> excludeNumbers = null)
+            int max = int.MaxValue)
         {
             if (min > max)
                 throw new ArgumentOutOfRangeException(nameof(min), min, "minValue is greater than maxValue");
 
             _minValue = min;
             _maxValue = max;
-            _excludeNumbers = excludeNumbers as List<int> ?? excludeNumbers.ToList();
 
             ReNewSeed();
         }
@@ -39,25 +59,25 @@ namespace ProjectCeleste.Misc.Utils.RandomGenerator
 
         [UsedImplicitly]
         [Pure]
-        public int Next()
+        public int Next([NotNull] ref IList<int> excludeNumbers)
         {
             lock (_syncLock)
             {
                 var countReTry = 0L;
                 var number = _random.Next(_minValue, _maxValue);
-                while (_excludeNumbers.Contains(number) && countReTry <= _randomMaxReTry)
+                while (excludeNumbers.Contains(number) && countReTry <= _randomMaxReTry)
                 {
                     countReTry++;
                     number = _random.Next(_minValue, _maxValue);
                 }
 
-                if (_excludeNumbers.Contains(number))
+                if (excludeNumbers.Contains(number))
                 {
                     ReNewSeed();
                     throw new IndexOutOfRangeException("Out of number, seed has been renewed");
                 }
 
-                _excludeNumbers.Add(number);
+                excludeNumbers.Add(number);
                 return number;
             }
         }
--- src/RandomGenerator/UniqueRandomLong.cs
+++ src/RandomGenerator/UniqueRandomLong.cs
@@ -8,26 +8,46 @@ using ProjectCeleste.Misc.Utils.Extension;
 namespace ProjectCeleste.Misc.Utils.RandomGenerator
 {
     [UsedImplicitly]
+    public class UniqueRandomLongGeneratorAlt
+    {
+        private IList<long> _excludeNumbers;
+        private readonly UniqueRandomLongGenerator _uniqueRandomLongGenerator;
+
+        public UniqueRandomLongGeneratorAlt([Range(long.MinValue, long.MaxValue - 1)]
+            long min = long.MinValue, [Range(long.MinValue + 1, long.MaxValue)]
+            long max = long.MaxValue,
+            [CanBeNull] IEnumerable<long> excludeNumbers = null)
+        {
+            _uniqueRandomLongGenerator = new UniqueRandomLongGenerator(min, max);
+            _excludeNumbers = excludeNumbers as List<long> ?? excludeNumbers?.ToList() ?? new List<long>();
+        }
+
+        [UsedImplicitly]
+        [Pure]
+        public long Next()
+        {
+            return _uniqueRandomLongGenerator.Next(ref _excludeNumbers);
+        }
+    }
+
+    [UsedImplicitly]
     public class UniqueRandomLongGenerator
     {
         private readonly long _randomMaxReTry = 25000 * Environment.ProcessorCount;
         private readonly object _syncLock = new object();
-        private readonly IList<long> _excludeNumbers;
         private readonly long _minValue;
         private readonly long _maxValue;
         private Random _random;
 
         public UniqueRandomLongGenerator([Range(long.MinValue, long.MaxValue - 1)]
             long min = long.MinValue, [Range(long.MinValue + 1, long.MaxValue)]
-            long max = long.MaxValue,
-            [CanBeNull] IEnumerable<long> excludeNumbers = null)
+            long max = long.MaxValue)
         {
             if (min > max)
                 throw new ArgumentOutOfRangeException(nameof(min), min, "minValue is greater than maxValue");
 
             _minValue = min;
             _maxValue = max;
-            _excludeNumbers = excludeNumbers as List<long> ?? excludeNumbers.ToList();
 
             ReNewSeed();
         }
@@ -40,25 +60,25 @@ namespace ProjectCeleste.Misc.Utils.RandomGenerator
 
         [UsedImplicitly]
         [Pure]
-        public long Next()
+        public long Next([NotNull] ref IList<long> excludeNumbers)
         {
             lock (_syncLock)
             {
                 var countReTry = 0L;
                 var number = _random.NextLong(_minValue, _maxValue);
-                while (_excludeNumbers.Contains(number) && countReTry <= _randomMaxReTry)
+                while (excludeNumbers.Contains(number) && countReTry <= _randomMaxReTry)
                 {
                     countReTry++;
                     number = _random.NextLong(_minValue, _maxValue);
                 }
 
-                if (_excludeNumbers.Contains(number))
+                if (excludeNumbers.Contains(number))
                 {
                     ReNewSeed();
                     throw new IndexOutOfRangeException("Out of number, seed has been renewed");
                 }
 
-                _excludeNumbers.Add(number);
+                excludeNumbers.Add(number);
                 return number;
             }
         }
